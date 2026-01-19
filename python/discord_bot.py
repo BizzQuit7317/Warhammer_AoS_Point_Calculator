b817960925822e832d927cac65a4f75a632b9820
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2026-01-19 17:56:16 +0000

    Update discord_bot.py

--- python/discord_bot.py
+++ python/discord_bot.py
@@ -37,6 +37,14 @@ async def get_points(ctx, faction, unit):
             await ctx.send(f"{points_check[1][0]} was found! it is {points_check[0]} points.")
     else:
         await ctx.send(f"{unit} is {points} points.")
-    
+
+@bot.command()
+@is_correct_channel()
+async def fetch_logs(ctx):
+    debug_channel = bot.get_channel(config['discord']['debug_channel_id'])
+    async for message in debug_channel.history(limit=100):
+        if message.content.startswith("[DBG]"):
+            print(f"{message.content}")
+
 
 bot.run(config['discord']['token'])
