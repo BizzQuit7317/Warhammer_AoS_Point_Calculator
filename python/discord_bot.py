b352808876a8e8b04a1b30d3d4b5165f50389baf
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2026-01-19 17:18:59 +0000

    Update discord_bot.py

--- python/discord_bot.py
+++ python/discord_bot.py
@@ -22,5 +22,17 @@ async def on_ready():
 @is_correct_channel()
 async def get_points(ctx, faction, unit):
     await ctx.send(f"Searching for {unit}, from the {faction}")
+    scraper = Scraper(faction)
+    points = scraper.collect_points(scraper.scrape(unit))
+    if points == 0:
+        await ctx.send(f"{unit} couldn't be found checking other names.....")
+        points_check =  scraper.collect_points_name_retry(unit)
+        if points_check[0] == 0:
+            await ctx.send(f"{unit} couldn't be found under any simmillar names.")
+        else:
+            await ctx.send(f"{points_check[1]} was found! it is {points} points.")
+    else:
+        await ctx.send(f"{unit} is {points} points.")
+    
 
 bot.run(config['discord']['token'])
