d0caeadc67e4c1f0aaeb93bc5f43165481fa6a55
Alby Brown <59367989+BizzQuit7317@users.noreply.github.com>
2026-01-19 20:36:25 +0000

    Update discord_agent.py

--- python/discord_agent.py
+++ python/discord_agent.py
@@ -1,3 +1,34 @@
 class Agent():
     def __init__(self):
         self.faction = None
+        self.factions_list = [ #A list of all faction from wahapedia to ominimise mistakes
+    "Cities of Sigmar"
+    "Daughters of Khaine"
+    "Fyreslayers"
+    "Idoneth Deepkin"
+    "Kharadron Overlords"
+    "Lumineth Realm-lords"
+    "Seraphon"
+    "Stormcast Eternals"
+    "Sylvaneth"
+    "Beasts of Chaos"
+    "Blades of Khorne"
+    "Disciples of Tzeentch"
+    "Hedonites of Slaanesh"
+    "Helsmiths of Hashut"
+    "Maggotkin of Nurgle"
+    "Skaven"
+    "Slaves to Darkness"
+    "Flesh-eater Courts"
+    "Nighthaunt"
+    "Ossiarch Bonereapers"
+    "Soulblight Gravelords"
+    "Bonesplitterz"
+    "Gloomspite Gitz"
+    "Ironjawz"
+    "Kruleboyz"
+    "Ogor Mawtribes"
+    "Sons of Behemat"
+    "Endless Spells"
+]
+
